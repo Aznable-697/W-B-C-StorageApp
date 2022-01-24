e0aa00d1dc21eb55fa9fe6ce6a3d2bbea1e3ca34
Matthew Whiteker <mattwhiteker@gmail.com>
2022-01-24 11:55:46 -0500

    Implemting Generic Class

---
+++ WiredBrainCoffee.StorageApp/Entities/Organization.cs
@@ -0,0 +1,10 @@
+﻿namespace WiredBrainCoffee.StorageApp.Entities
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } // string? = property can be Null**
+
+        public override string ToString() => $"Id: {Id}, FirstName: {Name}";
+    }
+}
--- WiredBrainCoffee.StorageApp/Program.cs
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -8,11 +8,20 @@ namespace WiredBrainCoffee.StorageApp
     {
         static void Main(string[] args)
         {
-            var employeeRepository = new EmployeeRepository();
+            var employeeRepository = new GenericRepository<Employee>();
             employeeRepository.Add(new Employee { FirstName = "John" });
-            employeeRepository.Add(new Employee { FirstName = "Gandolf" });
+            employeeRepository.Add(new Employee { FirstName = "Gandalf" });
             employeeRepository.Add(new Employee { FirstName = "Joey" });
             employeeRepository.Save();
+
+            Console.WriteLine($"-------------------------------------");
+
+            var organizationRepository = new GenericRepository<Organization>();
+            organizationRepository.Add(new Organization { Name = "Pluralsight" });
+            organizationRepository.Add(new Organization { Name = "SomeCoffeeBeanPlace" });
+            organizationRepository.Add(new Organization { Name = "The Business" });
+            organizationRepository.Save();
+
 
             Console.ReadLine();
 
